efc8c40f9436bfd61f055936189740561ee28287
Raghavan099 <venkatraghavan2013@gmail.com>
2018-11-05 18:13:18 +0530

    changed ncr

--- algos/combination.cpp
+++ algos/combination.cpp
@@ -1,34 +1,92 @@
+// g++ -std=c++14
+
+/*
+
+Today might be the chance to grasp the chance to let your talent bloom.
+May be tomorrow, the day after, or next year...
+May be even when you are 30. I'm not sure if physique has anything to do with it
+but if you think that it will never come, it probably never will.
+
+- Tooru Oikawa.
+
+*/
+
+
 #include<bits/stdc++.h>
+
 typedef long long ll;
+typedef long double lld;
 using namespace std;
-ll combi[1000006];
-long long binpow(long long val, long long deg, long long mod) {
-        if (!deg) return 1 % mod;
-        if (deg & 1) return binpow(val, deg - 1, mod) * val % mod;
-        long long res = binpow(val ,deg >> 1, mod);
-        return (res*res) % mod;
-}
-ll ncr(ll n,ll r){
-  ll ans1 = combi[n];
-  ll ans2 = (combi[n-r]*combi[r])%1000000007;
-  ans2 = binpow(ans2,1000000007-2,1000000007);
-  ll ans = (ans1*ans2)%1000000007;
-  return ans;
+
+#define sd(x) scanf("%d",&x)
+#define sd2(x,y) scanf("%d%d",&x,&y)
+#define sd3(x,y,z) scanf("%d%d%d",&x,&y,&z)
+#define endl "\n"
+#define fi first
+#define se second
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair(x,y)
+#define LET(x, a)  __typeof(a) x(a)
+#define foreach(it, v) for(LET(it, v.begin()); it != v.end(); it++)
+#define MEMS(a,b) memset(a,b,sizeof(a))
+#define _ ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define __ freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define all(c) c.begin(),c.end()
+#define inf 1000000000000000001
+
+#define tr(...) cout<<__FUNCTION__<<' '<<__LINE__<<" = ";trace(#__VA_ARGS__, __VA_ARGS__)
+
+template<typename S, typename T>
+ostream& operator<<(ostream& out,pair<S,T> const& p){out<<'('<<p.fi<<", "<<p.se<<')';return out;}
+
+template<typename T>
+ostream& operator<<(ostream& out,vector<T> const& v){
+ll l=v.size();for(ll i=0;i<l-1;i++)out<<v[i]<<' ';if(l>0)out<<v[l-1];return out;}
+
+template<typename T>
+void trace(const char* name, T&& arg1){cout<<name<<" : "<<arg1<<endl;}
+
+template<typename T, typename... Args>
+void trace(const char* names, T&& arg1, Args&&... args){
+const char* comma = strchr(names + 1, ',');cout.write(names, comma-names)<<" : "<<arg1<<" | ";trace(comma+1,args...);}
+
+#define int ll
+
+
+
+const int N = 4e5;
+const int MOD = 1e9 + 7;
+
+int fac[N + 10], ifac[N + 10];
+
+int power(int x, int t){
+    int ans = 1;
+    while(t > 0) {
+        if(t & 1) ans = 1LL * ans * x % MOD;
+        x = 1LL * x * x % MOD;
+        t >>= 1;
+    }
+    return ans;
 }
 
-int main(){
-  int n;
-  n = 100;
-  ll curr = 1;
-  combi[0] =1;
-  for(int i=1;i<=n;i++){
-    combi[i] = curr;
-    curr = (curr*(i+1))%1000000007;
-  }
-  ll a,b;
-  while(true){
-    cin>>a>>b;
-    cout<<ncr(a,b)<<endl;
-  }
-  return 0;
+void init_fac(){
+    fac[0] = 1;
+    for(int i = 1; i < N; i++){
+        fac[i] = (fac[i - 1] * i) % MOD;
+    }
+    ifac[N - 1] = power(fac[N - 1], MOD - 2);
+    for(int i = N - 1; i >= 1; i--){
+        ifac[i - 1] = (ifac[i] * i) % MOD;
+    }
+}
+
+int C(int n, int m)
+{
+	if(n < m) return 0;
+	return fac[n] * (1LL * ifac[m] * ifac[n - m] % MOD) % MOD;
+}
+
+
+int32_t main(){ _
+    init_fac();
 }
