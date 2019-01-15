ce8359dd9648074abce4c5ae6904276931d22536
Raghavan098 <venkartaghavan2013@gmail.com>
2019-01-15 10:33:47 +0530

    gaussian elimination

---
+++ algos/Gaussian.cpp
@@ -0,0 +1,58 @@
+struct Gaussian
+{
+	int no_of_bits = 32;
+	vector<int> v;
+	int set, origsize=0, redsize=0;
+
+	void push(int val)
+	{
+		origsize++;
+		if(val)
+			v.push_back(val);
+	}
+
+	void clear()
+	{
+		v.clear();
+		set=0, redsize=0;
+	}
+
+	void eliminate()
+	{
+		set = redsize = 0;
+		for(int bit=0;bit<=no_of_bits;bit++)
+		{
+			bool check=false;
+			for(int i=redsize;i<v.size();i++)
+			{
+				if((v[i]>>bit)&1)
+				{
+					swap(v[i], v[redsize]);
+					check=true;
+					break;
+				}
+			}
+			if(check)
+			{
+				for(int i=redsize+1;i<v.size();i++)
+				{
+					if((v[i]>>bit)&1)
+						v[i]^=v[redsize];
+				}
+				redsize++;
+			}
+		}
+		v.resize(redsize);
+		for(auto it:v)
+			set|=it;
+	}
+
+	Gaussian& operator =(Gaussian &orig)
+	{
+		v = orig.v;
+		set = orig.set;
+		redsize = orig.redsize;
+		origsize = orig.origsize;
+		return *this;
+	}
+};
