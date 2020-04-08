5608aae2d1303250b2d7f013edb622d995af1cdb
RaghavanGV <raghavan.gv@iiitb.org>
2020-04-08 14:30:49 +0530

    sa change

--- algos/suffix_array.cpp
+++ algos/suffix_array.cpp
@@ -78,3 +78,75 @@ struct SuffixArray {
 		return R.query(t0,t1-1);
 	}
 };
+
+
+//------------------------------------------use anything that is easy--------------------------
+typedef pair <ll, ll> pll;
+ 
+const int N = 2e5 + 10, mod = 1e9 + 7, inf = 1e9 + 42;
+// p[i] = rank of ith suffix!!!!
+ 
+struct SuffixArray {
+  const int L;
+  vector<int> s;
+  vector<vector<int> > P;
+  vector<pair<pair<int,int>,int> > M;
+ 
+  SuffixArray(const vector<int> &s) : L(s.size()), s(s), P(1, vector<int>(L, 0)), M(L) {
+    for (int i = 0; i < L; i++) P[0][i] = int(s[i]);
+    for (int skip = 1, level = 1; skip < L; skip *= 2, level++) {
+      P.push_back(vector<int>(L, 0));
+      for (int i = 0; i < L; i++)
+             M[i] = make_pair(make_pair(P[level-1][i], i + skip < L ? P[level-1][i + skip] : -1000), i);
+      sort(M.begin(), M.end());
+      for (int i = 0; i < L; i++)
+             P[level][M[i].second] = (i > 0 && M[i].first == M[i-1].first) ? P[level][M[i-1].second] : i;
+    }
+  }
+ 
+  vector<int> GetSuffixArray() { return P.back(); }
+  vector<int> GetInvSuffixArray() {
+      vector<int> ans(s.size());
+      auto p = GetSuffixArray();
+      for (int i = 0; i < (int)s.size(); i++) {
+          ans[p[i]] = i;
+      }
+      return ans;
+  }
+  // returns the length of the longest common prefix of s[i...L-1] and s[j...L-1]
+  int LongestCommonPrefix(int i, int j) {
+    int len = 0;
+    if (i == j) return L - i;
+    for (int k = P.size() - 1; k >= 0 && i < L && j < L; k--) {
+      if (P[k][i] == P[k][j]) {
+        i += 1 << k;
+        j += 1 << k;
+        len += 1 << k;
+      }
+    }
+    return len;
+  }
+};
+ 
+vector<int> lcp_construction(vector<int> const& s, vector<int> const& p) {
+    int n = s.size();
+    vector<int> rank(n, 0);
+    for (int i = 0; i < n; i++)
+        rank[p[i]] = i;
+ 
+    int k = 0;
+    vector<int> lcp(n-1, 0);
+    for (int i = 0; i < n; i++) {
+        if (rank[i] == n - 1) {
+            k = 0;
+            continue;
+        }
+        int j = p[rank[i] + 1];
+        while (i + k < n && j + k < n && s[i+k] == s[j+k])
+            k++;
+        lcp[rank[i]] = k;
+        if (k)
+            k--;
+    }
+    return lcp;
+}
